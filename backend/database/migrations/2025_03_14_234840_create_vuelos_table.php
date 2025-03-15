9dd3c4483f65f02cb81dd4e12a8f95b67cf7321b
DESKTOP-RD568GF\Killer <jebular481@gmail.com>
2025-03-15 01:39:22 +0100

    Instalacion frameworks

---
+++ backend/database/migrations/2025_03_14_234840_create_vuelos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vuelos', function (Blueprint $table) {
+            $table->id();
+            $table->string('origen');
+            $table->string('destino');
+            $table->date('fecha');
+            $table->decimal('precio', 10, 2);
+            $table->integer('asientos_disponibles');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vuelos');
+    }
+};
